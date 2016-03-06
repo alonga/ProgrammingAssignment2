e5ff9809cc80c38015da33b03a9bb7b2dab51eb9
Alon Gur-Arie <ag4611180>
2016-03-06 11:24:07 +0200

    Adding Extended Matrix functions to local rep

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a matrix object that it and its  can be cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i  <- NULL
+  set  <- function(y){
+    x <<- y
+    i <<- NULL 
+  }
+  get  <- function() x
+  setinverse  <- function(inverse) i  <<- inverse
+  getinverse  <- function() i
+  list(set= set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of the matrix. If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse that exists in the cache.
+## C is  a variable used to ensure access to makeCacheMatrix  elements
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  C<-makeCacheMatrix(x)
+  i  <- C$getinverse()
+  if (!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data  <- C$get()
+  i  <- solve(data, ...)
+  C$setinverse(i)
+  i
 }
